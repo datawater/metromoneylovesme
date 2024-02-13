8ec0ed201ab174c465173640fa7ab6d8c623fd5c
datawater <86855332+datawater@users.noreply.github.com>
2024-02-13 15:27:53 +0400

    Added the decypted dumps

--- hexdump.py
+++ hexdump.py
@@ -1,6 +1,8 @@
-hex_string_metro = open("decrypt.txt", "r").read().replace(" ", "").replace("\n", "")
+import sys
+
+hex_string_metro = open(sys.argv[1], "r").read().replace(" ", "").replace("\n", "")
 hex_data_metro = bytearray.fromhex(hex_string_metro)
 
-f_metro = open("data/metromani.dat", "wb")
+f_metro = open("data/metromani1.dat", "wb")
 
 f_metro.write(hex_data_metro)
